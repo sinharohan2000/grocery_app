6cb67bf63668c5a7dda18d082285a45869f370ac
Rohan Sinha <sinharohan2000@gmail.com>
2020-09-30 20:21:10 +0545

    Addition
    Signed-off-by: Rohan Sinha <sinharohan2000@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class GroceryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Grocery App',
       theme: ThemeData(
         backgroundColor: Color(0xfff4e3),
         primarySwatch: Colors.blue,
