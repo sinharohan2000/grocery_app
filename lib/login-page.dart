107975699dfbe6eaf1b31b8aef58c8def79804cc
rishav079 <rishavrauniyar88@gmail.com>
2020-10-18 09:32:23 +0545

    initial

--- lib/login-page.dart
+++ lib/login-page.dart
@@ -7,6 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:groceryapp/registration-screen.dart';
 import 'home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const id = 'login_screen';
@@ -18,177 +19,189 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
 
   String email,password;
+  bool showSpinner=false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                      height: 50.0,
-                      child: Image(
-                        image: AssetImage('images/food-delivery.png'),
-                        width: 50.0,
-                      )
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:25.0),
-                  child: Text(
-                    'Login',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontFamily: 'SansitaSwashed',
-                      color: Colors.brown[500],
-                      fontSize: 30.0,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                        height: 50.0,
+                        child: Image(
+                          image: AssetImage('images/food-delivery.png'),
+                          width: 50.0,
+                        )
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                email=value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Colors.brown[700], width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Colors.brown[700], width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value){
-                //Do something with the user input.
-                password=value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password.',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Colors.brown[700], width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  BorderSide(color: Colors.brown[700], width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, bottom: 8.0),
-              child: SizedBox(
-                child: new RichText(
-                  textAlign: TextAlign.right,
-                  text: new TextSpan(
-                    children: [
-                      new TextSpan(
-                        text: 'Don\'t have an account? ',
-                        style: new TextStyle(color: Colors.black),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:25.0),
+                    child: Text(
+                      'Login',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontFamily: 'SansitaSwashed',
+                        color: Colors.brown[500],
+                        fontSize: 30.0,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
                       ),
-                      new TextSpan(
-                        text: 'Signup!',
-                        style: new TextStyle(color: Colors.blue),
-                        recognizer: new TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.pushNamed(context, RegistrationScreen.id);
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 11.0),
-              child: Material(
-                color: Colors.brown[700],
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () async {
-                    await signin(email, password);
-                    Navigator.pushNamed(context, HomeScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(
-                      color: Colors.white,
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-              child: Text(
-                'Or',
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
                 textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email=value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.brown[700], width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.brown[700], width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-              child: Material(
-                color: Colors.brown[200],
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () =>
-                      googleSignIn().whenComplete(() =>
-                          Navigator.pushNamed(context, HomeScreen.id)),
-                  minWidth: 10.0,
-                  height: 20.0,
-                  child: Text(
-                    'Login with Google',
-                    style: TextStyle(
-                      color: Colors.brown[900],
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value){
+                  //Do something with the user input.
+                  password=value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your password.',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.brown[700], width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.brown[700], width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom: 8.0),
+                child: SizedBox(
+                  child: new RichText(
+                    textAlign: TextAlign.right,
+                    text: new TextSpan(
+                      children: [
+                        new TextSpan(
+                          text: 'Don\'t have an account? ',
+                          style: new TextStyle(color: Colors.black),
+                        ),
+                        new TextSpan(
+                          text: 'Signup!',
+                          style: new TextStyle(color: Colors.blue),
+                          recognizer: new TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pushNamed(context, RegistrationScreen.id);
+                            },
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 11.0),
+                child: Material(
+                  color: Colors.brown[700],
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner=true;
+                      });
+                      final user =await signin(email, password);
+                      if (user!=null) {
+                        Navigator.pushNamed(context, HomeScreen.id);
+                      }
+                      setState(() {
+                        showSpinner=false;
+                      });
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Text(
+                  'Or',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                child: Material(
+                  color: Colors.brown[200],
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () =>
+                        googleSignIn().whenComplete(() =>
+                            Navigator.pushNamed(context, HomeScreen.id)),
+                    minWidth: 10.0,
+                    height: 20.0,
+                    child: Text(
+                      'Login with Google',
+                      style: TextStyle(
+                        color: Colors.brown[900],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/registration-screen.dart
+++ lib/registration-screen.dart
@@ -4,6 +4,7 @@ import 'package:groceryapp/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id='registration_screen';
@@ -15,120 +16,139 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> with TickerProviderStateMixin {
   final _auth=FirebaseAuth.instance;
   String email,password;
+  bool showSpinner=false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 50.0,
-                    child: Image.asset('images/food-delivery.png'),
-                  ),
-                ),
-                Container(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Text(
-                    'Signup',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontFamily: 'SansitaSwashed',
-                      color: Colors.brown[500],
-                      fontSize: 30.0,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 50.0,
+                      child: Image.asset('images/food-delivery.png'),
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.brown[700], width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.brown[700], width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: Text(
+                      'Signup',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontFamily: 'SansitaSwashed',
+                        color: Colors.brown[500],
+                        fontSize: 30.0,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                password=value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.brown[700], width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.brown[700], width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.brown[700],
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () =>
-                      signup(email, password).whenComplete(() =>
-                          Navigator.pushNamed(context, HomeScreen.id)),
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown[700], width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown[700], width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password=value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown[700], width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown[700], width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  color: Colors.brown[700],
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner=true;
+                      });
+                      try{
+                        final newUser= await _auth.createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        if(newUser!=null){
+                          Navigator.pushNamed(context, HomeScreen.id);
+                        }
+                        setState(() {
+                          showSpinner=false;
+                        });
+                      }
+                      catch(e){
+                        print(e);
+                      }
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
