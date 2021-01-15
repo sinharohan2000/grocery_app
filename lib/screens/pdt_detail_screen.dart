c17d78a5f7d02d8a95a6a505b5dd73e9828025ce
rishav079 <rishavrauniyar88@gmail.com>
2021-01-15 09:00:38 +0530

    added description final

---
+++ lib/screens/pdt_detail_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/export.dart';
+import 'package:groceryapp/models/product-model.dart';
+
+class DetailPage extends StatelessWidget {
+  static String id = "detail_screen";
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedPdt = Provider.of<Products>(context).findById(productId);
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedPdt.productName),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.network(loadedPdt.imgUrl),
+            ),
+          ),
+          Text(
+            'Price:\₹${loadedPdt.price}',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              '${loadedPdt.description}',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Scaffold.of(context).showSnackBar(SnackBar(
+          //   duration: Duration(seconds: 3),
+          //   content: Text('Item Added to Cart'),
+          // ));
+          cart.addItem(productId, loadedPdt.productName
+              , loadedPdt.price);
+        },
+        child: Icon(
+          Icons.shopping_cart,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
